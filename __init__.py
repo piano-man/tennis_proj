0d607f1b2c2639d1ba1f267824665150e304a320
piano-man <icm2015003@iiita.ac.in>
2017-06-26 20:36:24 +0530

    push message protocol initiated

--- __init__.py
+++ __init__.py
@@ -2,17 +2,27 @@ from lxml import html
 from bs4 import BeautifulSoup
 import requests
 from flask import Flask,render_template
+from flask_socketio import SocketIO
 app=Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
+    return render_template("try.html")
+
+@socketio.on('subscription_id')
+def socfunc(json):
+    print(json)
+
+
+@app.route('/schedule')
+def schedule():
     page = requests.get("http://www.espn.com/tennis/schedule")
     soup = BeautifulSoup(page.content,'html.parser')
     soup.prettify()
-    current = []
-    upcoming = []
-    completed = []
     tables = soup.find_all("table")
+    return render_template("")
     for tr in tables[0].find_all("tr")[2:]:
         tds = tr.find_all("td")
         print("Date: %s, Name: %s, Location: %s"%(tds[0].text, tds[1].text, tds[2].text))
@@ -22,14 +32,6 @@ def index():
 
 
 
-
-
- 
-
-
-
-
-
-
 if __name__=="__main__":
-    app.run()
+    socketio.run(app)
+
